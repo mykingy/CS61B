8fdbae8f0b759d951b1982119759cf164498dce1
mykingy <37398810+mykingy@users.noreply.github.com>
2018-10-19 23:35:40 -0400

    Finish calcNetForceExertedByX and calcNetForceExertedByY

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -29,8 +29,8 @@ public class Planet{
 
 
   public double calcDistance(Planet p){
-    double dx = this.xxPos - p.xxPos;
-    double dy = this.yyPos - p.yyPos;
+    double dx = p.xxPos - this.xxPos;
+    double dy = p.yyPos - this.yyPos;
     double R2 = dx * dx + dy * dy;
     double R = Math.sqrt(R2);
     return R;
@@ -43,25 +43,38 @@ public class Planet{
   }
 
   public double calcForceExertedByX(Planet p){
-    double dx = this.xxPos - p.xxPos;
-    double forceX;
-    if (dx >= 0){
-      forceX = this.calcForceExertedBy(p) * dx / this.calcDistance(p);
-    } forceX = - (this.calcForceExertedBy(p) * dx / this.calcDistance(p));
+    double dx = p.xxPos - this.xxPos;
+    double forceX = this.calcForceExertedBy(p) * dx / this.calcDistance(p);
 
     return forceX;
   }
 
   public double calcForceExertedByY(Planet p){
-    double dy = this.yyPos - p.yyPos;
-    double forceY;
-    if (dy >= 0){
-      forceY = this.calcForceExertedBy(p) * dy / this.calcDistance(p);
-    } forceY = this.calcForceExertedBy(p) * (-dy) / this.calcDistance(p);
+    double dy = p.yyPos - this.xxPos;
+    double forceY = this.calcForceExertedBy(p) * dy / this.calcDistance(p);
 
     return forceY;
   }
 
+public double calcNetForceExertedByX(Planet[] stars){
+  double forceNetX = 0.0;
+  for(int i = 0; i < stars.length; i = i + 1){
+    if (stars[i] != this){
+      forceNetX = forceNetX + this.calcForceExertedByX(stars[i]);
+    }
+  }
+    return forceNetX;
+}
+
+public double calcNetForceExertedByY(Planet[] stars){
+  double forceNetY = 0.0;
+  for(int i = 0; i < stars.length; i = i + 1){
+    if (stars[i] != this){
+      forceNetY = forceNetY + this.calcForceExertedByY(stars[i]);
+    }
+  }
+    return forceNetY;
+}
 
 
 }
