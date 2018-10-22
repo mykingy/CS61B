f626f277df234a7aede3a797296377f240086a4b
mykingy <37398810+mykingy@users.noreply.github.com>
2018-10-21 22:09:25 -0400

    finish collecting input part

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -26,6 +26,15 @@ public class NBody{
 
     return stars;
 
+  }
+
+  public static void main(String[] args){
+    double T = Double.parseDouble(args[0]);
+    double dt = Double.parseDouble(args[1]);
+    String filename = args[2];
+    double unra = this.readRadius(filename);
+    double pl = this.readPlanets(filename);
+
 
   }
 }
